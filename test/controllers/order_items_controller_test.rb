2714d13a842f44df5f963dc6b60b2d80140e9db9
John Romkey <58883+romkey@users.noreply.github.com>
2020-04-20 15:42:16 -0700

    mores plurals

--- test/controllers/order_items_controller_test.rb
+++ test/controllers/order_items_controller_test.rb
@@ -20,7 +20,7 @@ class OrderItemsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create order_item" do
     assert_difference('OrderItem.count') do
-      post order_items_url, params: { order_item: { max_quantity: @order_item.max_quantity, min_quantity: @order_item.min_quantity,  shared_order: shared_order(:one), user: user(:one), available_item: available_item(:one) } }
+      post order_items_url, params: { order_item: { max_quantity: @order_item.max_quantity, min_quantity: @order_item.min_quantity,  shared_order: shared_orders(:one), user: users(:one), available_item: available_items(:one) } }
     end
 
     assert_redirected_to order_item_url(OrderItem.last)
@@ -37,7 +37,7 @@ class OrderItemsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update order_item" do
-    patch order_item_url(@order_item), params: { order_item: { max_quantity: @order_item.max_quantity, min_quantity: @order_item.min_quantity, shared_order: shared_order(:one), user: user(:one), available_item: available_item(:one) } }
+    patch order_item_url(@order_item), params: { order_item: { max_quantity: @order_item.max_quantity, min_quantity: @order_item.min_quantity, shared_order: shared_orders(:one), user: users(:one), available_item: available_items(:one) } }
     assert_redirected_to order_item_url(@order_item)
   end
 
