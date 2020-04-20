96d4c30eb1c4c99a3ae7d4cd773abe25b594059d
John Romkey <58883+romkey@users.noreply.github.com>
2020-04-19 20:58:00 -0700

    foreign keys

--- db/migrate/20200418041816_create_shared_order_user_table.rb
+++ db/migrate/20200418041816_create_shared_order_user_table.rb
@@ -1,8 +1,8 @@
 class CreateSharedOrderUserTable < ActiveRecord::Migration[6.0]
   def change
     create_table :shared_orders_users do |t|
-      t.belongs_to :user
-      t.belongs_to :shared_order
+      t.belongs_to :user, foreign_key: true
+      t.belongs_to :shared_order, foreign_key: true
     end
   end
 end
